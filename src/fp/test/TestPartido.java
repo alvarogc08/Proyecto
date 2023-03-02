aa62da559467db4581b0a3a85e08214e44783b31
alvarogc08 <alvarogomc31@gmail.com>
2023-03-03 00:11:22 +0100

    Entrega 1.

---
+++ src/fp/test/TestPartido.java
@@ -0,0 +1,16 @@
+package fp.test;
+
+import fp.tipos.*;
+
+public class TestPartido {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		Partido p2=new Partido("Barcelona", true, 85.6);
+		System.out.println(p2);
+
+	}
+
+}
